ed406cdfd9bbff305a856e19666ef7b188a68312
BrunoSilva03 <78625466+BrunoSilva03@users.noreply.github.com>
2022-11-22 09:22:31 -0300

    Add files via upload

---
+++ GoogleHelloWorld/src/test/java/API/GoogleHelloWorld/GoogleHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package API.GoogleHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
